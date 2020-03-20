7dba6de44432d25951e9a1c7b7fc0ec5882b6357
andrew <andrew_mokryj@ukr.net>
2020-03-20 21:27:24 +0200

    'Signup works

--- lib/views/signup_page.dart
+++ lib/views/signup_page.dart
@@ -6,7 +6,16 @@ import 'package:flutterappweb/model/login_model.dart';
 import 'package:flutterappweb/model/user_model.dart';
 import 'package:provider/provider.dart';
 
-class SignupPage extends StatelessWidget{
+class SignupPage extends StatefulWidget{
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  User newUser = User();
+
   @override
   Widget build(BuildContext context) {
     SchedulerBinding.instance.addPostFrameCallback((_) {
@@ -16,7 +25,7 @@ class SignupPage extends StatelessWidget{
     return Scaffold(
         body: SafeArea(
           child: Stack(children: <Widget>[Form(
-//          key: _formKey,
+          key: _formKey,
             child: SingleChildScrollView(padding: EdgeInsets.all(10.0),
               child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
@@ -37,9 +46,7 @@ class SignupPage extends StatelessWidget{
 //                    fieldError('username', val.isEmpty ? AppLocalizations.of(
 //                      context).localizedText('signInNoLoginError') : null),
                     autovalidate: true,
-                    onSaved: (val) =>
-                    {}
-                    ,
+                    onSaved: (val) => newUser.username = val,
                   ),
                   const SizedBox(height: 20.0),
                   TextFormField(
@@ -58,9 +65,7 @@ class SignupPage extends StatelessWidget{
 //                    fieldError('password', val.isEmpty ? AppLocalizations.of(
 //                      context).localizedText('signInNoPasswordError') : null),
                     autovalidate: true,
-                    onSaved: (val) =>
-                    {}
-                    ,
+                    onSaved: (val) => newUser.password = val,
                   ),
                   const SizedBox(height: 20.0),
                   FlatButton(
@@ -69,10 +74,11 @@ class SignupPage extends StatelessWidget{
                       'Зарегистрироваться'
                     ),
                     onPressed: () {
-                      var success = DBProvider.db.newUser(User(username: "admin", password:"admin"));
-                      if(success) {
-                        Navigator.of(context).pushNamed('/login');
-                      }
+//                      var success = DBProvider.db.newUser(User(username: "admin", password:"admin"));
+//                      if(success) {
+//                        Navigator.of(context).pushNamed('/login');
+//                      }
+                      _submit();
                     }
                   ),
                 ],
@@ -81,5 +87,13 @@ class SignupPage extends StatelessWidget{
           ],)
         ),
       );
+  }
+
+  void _submit() {
+    final FormState form = _formKey.currentState;
+    form.save();
+    if (DBProvider.db.newUser(newUser)) {
+      Navigator.of(context).pushNamed('/login');
+    }
   }
 }
