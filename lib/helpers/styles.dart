df4000a4cd340d914a66765d77ee0dbf6543e05a
andrew <andrew_mokryj@ukr.net>
2020-04-05 22:48:46 +0300

    Login page adaptive

--- lib/helpers/styles.dart
+++ lib/helpers/styles.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const fieldTextStyle = const TextStyle(fontFamily: 'SF UI Text', fontSize: 14, color: Color.fromRGBO(128, 128, 128, 1.0));
+const fieldTextStyle = const TextStyle(fontFamily: 'SF UI Text', fontSize: 16, color: Color.fromRGBO(50, 50, 50, 1.0));
 
 class AppDecoration extends InputDecoration {
   const AppDecoration({
@@ -12,9 +12,9 @@ class AppDecoration extends InputDecoration {
   }) : super(
     contentPadding: contentPadding,
     floatingLabelBehavior: (hasFloatingPlaceholder ?? false) ? FloatingLabelBehavior.never : FloatingLabelBehavior.always,
-    border: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(230, 230, 230, 1.0))),
-    enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(230, 230, 230, 1.0))),
-    focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(128, 128, 128, 1.0))),
+    border: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(150, 150, 150, 1.0))),
+    enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(150, 150, 150, 1.0))),
+    focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(75, 75, 75, 1.0))),
     focusedErrorBorder: const OutlineInputBorder(borderSide: BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 1.0))),
 //    errorBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(0, 0, 0, 1.0))),
 
--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutterappweb/database/database.dart';
+import 'package:flutterappweb/helpers/constants.dart';
 import 'package:flutterappweb/helpers/styles.dart';
 import 'package:flutterappweb/helpers/utils.dart';
 import 'package:flutterappweb/model/notifiers/login_notifier.dart';
@@ -31,66 +32,71 @@ class _LoginPageState extends State<LoginPage> {
       },
       child: Scaffold(
         body: SafeArea(
-          child: Stack(children: <Widget>[Form(
-          key: _formKey,
-            child: SingleChildScrollView(padding: EdgeInsets.all(10.0),
-              child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Text('Введите адрес электронной почты и пароль',
-                    style: TextStyle(fontSize: 12.0)),
-                  const SizedBox(height: 12.0),
-                  TextFormField(
-                    style: fieldTextStyle,
-                    enabled: true,
-                  decoration: AppDecoration(
-                    labelText: "",
-                    hintText: "",
-                  ),
-                    keyboardType: TextInputType.emailAddress,
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: Container(
+                  width: w > smallLimit ? 500 : w,
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text('Введите имя пользователя и пароль',
+                        style: TextStyle(fontSize: 14.0)),
+                      const SizedBox(height: 12.0),
+                      TextFormField(
+                        style: fieldTextStyle,
+                        enabled: true,
+                        decoration: AppDecoration(
+                          labelText: "Имя пользователя",
+                          hintText: "",
+                        ),
+                        keyboardType: TextInputType.emailAddress,
 //                  validator: (val) =>
 //                    fieldError('username', val.isEmpty ? AppLocalizations.of(
 //                      context).localizedText('signInNoLoginError') : null),
-                    autovalidate: true,
-                    onSaved: (val) => newUser.username = val,
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    style: fieldTextStyle,
-                    enabled: true,
-                  decoration: AppDecoration(
-                    labelText: 'Password',
-                    hintText: 'Пароль для входа'
-                  ),
-                    obscureText: true,
+                        autovalidate: true,
+                        onSaved: (val) => newUser.username = val,
+                      ),
+                      const SizedBox(height: 20.0),
+                      TextFormField(
+                        style: fieldTextStyle,
+                        enabled: true,
+                        decoration: AppDecoration(
+                          labelText: 'Пароль',
+                          hintText: 'Пароль для входа'
+                        ),
+                        obscureText: true,
 //                  validator: (val) =>
 //                    fieldError('password', val.isEmpty ? AppLocalizations.of(
 //                      context).localizedText('signInNoPasswordError') : null),
-                    autovalidate: true,
-                    onSaved: (val) => newUser.password = val,
+                        autovalidate: true,
+                        onSaved: (val) => newUser.password = val,
 
-                  ),
-                  const SizedBox(height: 20.0),
-                  FlatButton(
-                    //text: 'Войти',
-                    child: Text(
-                      'Войти'
-                    ),
-                    onPressed: () => _submit(),
+                      ),
+                      const SizedBox(height: 20.0),
+                      FlatButton(
+                        //text: 'Войти',
+                        child: Text(
+                          'Войти'
+                        ),
+                        onPressed: () => _submit(),
 
+                      ),
+                      const SizedBox(height: 11.0),
+                      const SizedBox(height: 27.0),
+                      FlatButton(
+                        child: Text('Зарегистрироваться',
+                          style: TextStyle(fontFamily: 'PT Sans',
+                            fontSize: 14.0,
+                            color: Colors.black),),
+                        onPressed: () => Navigator.of(context).pushNamed('/signup'),
+                      )
+                    ],
                   ),
-                  const SizedBox(height: 11.0),
-                  const SizedBox(height: 27.0),
-                  FlatButton(
-                    child: Text('Зарегистрироваться',
-                      style: TextStyle(fontFamily: 'PT Sans',
-                        fontSize: 14.0,
-                        color: Colors.black),),
-                    onPressed: () => Navigator.of(context).pushNamed('/signup'),
-                  )
-                ],
+                ),
               ))
           ),
-          ],)
         ),
       ),
     );
