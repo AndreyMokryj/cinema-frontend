1f9069012c0e8fb2b65ce5b9184f528057a2bb65
andrew <andrew_mokryj@ukr.net>
2020-04-07 13:42:55 +0300

    Menu - add 'hide' button

--- lib/views/menu_widget.dart
+++ lib/views/menu_widget.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutterappweb/helpers/constants.dart';
+import 'package:flutterappweb/helpers/utils.dart';
 import 'package:flutterappweb/model/notifiers/login_notifier.dart';
 import 'package:provider/provider.dart';
 
 class MenuWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    double w = getWidth(context);
+
     return Container(
-      color: Colors.grey,
+      color: Color.fromRGBO(180, 180, 180, 1),
       child: ListView(
         children: <Widget>[
+          w <= largeLimit ? Align(
+            alignment: Alignment.centerLeft,
+            child: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            ),
+          ) : Container(),
           ListTile(
             leading: Icon(Icons.home),
             title: Text("Главная"),
